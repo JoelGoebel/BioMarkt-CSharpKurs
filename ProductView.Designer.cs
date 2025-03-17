a9b6441f20335f6be563439a3028e3825e73dedd
JoelGoebel <joel.goebel.jg@web.de>
2025-03-17 14:10:28 +0100

    Changes

--- ProductView.Designer.cs
+++ ProductView.Designer.cs
@@ -30,13 +30,19 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProductView));
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tb_Name = new System.Windows.Forms.TextBox();
+            this.tb_Marke = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.tb_Preis = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cb_Kategorie = new System.Windows.Forms.ComboBox();
+            this.btn_ProductSave = new System.Windows.Forms.Button();
+            this.btn_ProductEdit = new System.Windows.Forms.Button();
+            this.btn_ProductClear = new System.Windows.Forms.Button();
+            this.btn_ProductDelete = new System.Windows.Forms.Button();
+            this.dgw_ProductTable = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw_ProductTable)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -51,25 +57,25 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Name";
             // 
-            // textBox1
+            // tb_Name
             // 
-            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.ForeColor = System.Drawing.Color.White;
-            this.textBox1.Location = new System.Drawing.Point(82, 11);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(168, 20);
-            this.textBox1.TabIndex = 1;
+            this.tb_Name.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.tb_Name.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_Name.ForeColor = System.Drawing.Color.White;
+            this.tb_Name.Location = new System.Drawing.Point(82, 11);
+            this.tb_Name.Name = "tb_Name";
+            this.tb_Name.Size = new System.Drawing.Size(276, 20);
+            this.tb_Name.TabIndex = 1;
             // 
-            // textBox2
+            // tb_Marke
             // 
-            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox2.ForeColor = System.Drawing.Color.White;
-            this.textBox2.Location = new System.Drawing.Point(82, 37);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(168, 20);
-            this.textBox2.TabIndex = 3;
+            this.tb_Marke.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.tb_Marke.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_Marke.ForeColor = System.Drawing.Color.White;
+            this.tb_Marke.Location = new System.Drawing.Point(82, 37);
+            this.tb_Marke.Name = "tb_Marke";
+            this.tb_Marke.Size = new System.Drawing.Size(276, 20);
+            this.tb_Marke.TabIndex = 3;
             // 
             // label2
             // 
@@ -95,15 +101,15 @@
             this.label3.TabIndex = 4;
             this.label3.Text = "Kategorie";
             // 
-            // textBox4
+            // tb_Preis
             // 
-            this.textBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
-            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox4.ForeColor = System.Drawing.Color.White;
-            this.textBox4.Location = new System.Drawing.Point(82, 89);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(168, 20);
-            this.textBox4.TabIndex = 7;
+            this.tb_Preis.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.tb_Preis.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_Preis.ForeColor = System.Drawing.Color.White;
+            this.tb_Preis.Location = new System.Drawing.Point(82, 89);
+            this.tb_Preis.Name = "tb_Preis";
+            this.tb_Preis.Size = new System.Drawing.Size(276, 20);
+            this.tb_Preis.TabIndex = 7;
             // 
             // label4
             // 
@@ -117,16 +123,84 @@
             this.label4.TabIndex = 6;
             this.label4.Text = "Preis";
             // 
-            // comboBox1
+            // cb_Kategorie
             // 
-            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
-            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.comboBox1.ForeColor = System.Drawing.Color.White;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(82, 62);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(168, 21);
-            this.comboBox1.TabIndex = 8;
+            this.cb_Kategorie.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.cb_Kategorie.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cb_Kategorie.ForeColor = System.Drawing.Color.White;
+            this.cb_Kategorie.FormattingEnabled = true;
+            this.cb_Kategorie.Items.AddRange(new object[] {
+            "Obst",
+            "Gemüse",
+            "Fleisch",
+            "Fisch",
+            "Drogerie",
+            "Getränke"});
+            this.cb_Kategorie.Location = new System.Drawing.Point(82, 62);
+            this.cb_Kategorie.Name = "cb_Kategorie";
+            this.cb_Kategorie.Size = new System.Drawing.Size(276, 21);
+            this.cb_Kategorie.TabIndex = 8;
+            // 
+            // btn_ProductSave
+            // 
+            this.btn_ProductSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.btn_ProductSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_ProductSave.ForeColor = System.Drawing.Color.White;
+            this.btn_ProductSave.Location = new System.Drawing.Point(79, 127);
+            this.btn_ProductSave.Name = "btn_ProductSave";
+            this.btn_ProductSave.Size = new System.Drawing.Size(89, 23);
+            this.btn_ProductSave.TabIndex = 9;
+            this.btn_ProductSave.Text = "Speichern";
+            this.btn_ProductSave.UseVisualStyleBackColor = false;
+            this.btn_ProductSave.Click += new System.EventHandler(this.btn_ProductSave_Click);
+            // 
+            // btn_ProductEdit
+            // 
+            this.btn_ProductEdit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.btn_ProductEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_ProductEdit.ForeColor = System.Drawing.Color.White;
+            this.btn_ProductEdit.Location = new System.Drawing.Point(174, 127);
+            this.btn_ProductEdit.Name = "btn_ProductEdit";
+            this.btn_ProductEdit.Size = new System.Drawing.Size(89, 23);
+            this.btn_ProductEdit.TabIndex = 10;
+            this.btn_ProductEdit.Text = "Bearbeiten";
+            this.btn_ProductEdit.UseVisualStyleBackColor = false;
+            this.btn_ProductEdit.Click += new System.EventHandler(this.btn_ProductEdit_Click);
+            // 
+            // btn_ProductClear
+            // 
+            this.btn_ProductClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.btn_ProductClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_ProductClear.ForeColor = System.Drawing.Color.White;
+            this.btn_ProductClear.Location = new System.Drawing.Point(269, 127);
+            this.btn_ProductClear.Name = "btn_ProductClear";
+            this.btn_ProductClear.Size = new System.Drawing.Size(89, 23);
+            this.btn_ProductClear.TabIndex = 11;
+            this.btn_ProductClear.Text = "Felder leeren";
+            this.btn_ProductClear.UseVisualStyleBackColor = false;
+            this.btn_ProductClear.Click += new System.EventHandler(this.btn_ProductClear_Click);
+            // 
+            // btn_ProductDelete
+            // 
+            this.btn_ProductDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.btn_ProductDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_ProductDelete.ForeColor = System.Drawing.Color.White;
+            this.btn_ProductDelete.Location = new System.Drawing.Point(364, 127);
+            this.btn_ProductDelete.Name = "btn_ProductDelete";
+            this.btn_ProductDelete.Size = new System.Drawing.Size(89, 23);
+            this.btn_ProductDelete.TabIndex = 12;
+            this.btn_ProductDelete.Text = "Löschen";
+            this.btn_ProductDelete.UseVisualStyleBackColor = false;
+            this.btn_ProductDelete.Click += new System.EventHandler(this.btn_ProductDelete_Click);
+            // 
+            // dgw_ProductTable
+            // 
+            this.dgw_ProductTable.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.dgw_ProductTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgw_ProductTable.Location = new System.Drawing.Point(12, 156);
+            this.dgw_ProductTable.Name = "dgw_ProductTable";
+            this.dgw_ProductTable.Size = new System.Drawing.Size(775, 293);
+            this.dgw_ProductTable.TabIndex = 13;
             // 
             // ProductView
             // 
@@ -134,19 +208,25 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(799, 461);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.dgw_ProductTable);
+            this.Controls.Add(this.btn_ProductDelete);
+            this.Controls.Add(this.btn_ProductClear);
+            this.Controls.Add(this.btn_ProductEdit);
+            this.Controls.Add(this.btn_ProductSave);
+            this.Controls.Add(this.cb_Kategorie);
+            this.Controls.Add(this.tb_Preis);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.tb_Marke);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tb_Name);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "ProductView";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Produkte";
+            ((System.ComponentModel.ISupportInitialize)(this.dgw_ProductTable)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -155,12 +235,17 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tb_Name;
+        private System.Windows.Forms.TextBox tb_Marke;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox tb_Preis;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cb_Kategorie;
+        private System.Windows.Forms.Button btn_ProductSave;
+        private System.Windows.Forms.Button btn_ProductEdit;
+        private System.Windows.Forms.Button btn_ProductClear;
+        private System.Windows.Forms.Button btn_ProductDelete;
+        private System.Windows.Forms.DataGridView dgw_ProductTable;
     }
 }
--- ProductView.cs
+++ ProductView.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,40 @@ namespace ProNaturBiomarkt
 {
     public partial class ProductView: Form
     {
+        private SqlConnection DatabaseConnection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\jgadmin\\Documents\\ProNaturBiomarkt.mdf;Integrated Security=True;Connect Timeout=30");
+
+
         public ProductView()
         {
             InitializeComponent();
+
+            //Start
+            DatabaseConnection.Open();
+            DatabaseConnection.Close();
+        }
+
+        private void btn_ProductSave_Click(object sender, EventArgs e)
+        {
+            string ProductName = tb_Name.Text;
+            string ProductBrand = tb_Marke.Text;
+            string ProductKategorie = cb_Kategorie.Text;
+            float ProductPrice = float.Parse(tb_Preis.Text);
+            Console.WriteLine(ProductName);
+        }
+
+        private void btn_ProductEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_ProductClear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_ProductDelete_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
